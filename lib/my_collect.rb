c2e3410f4e0b63905cb9aebf819f976148aae709
Sav Younts <savyounts@gmail.com>
2018-08-03 20:44:24 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0 
   collection_array = []
   while i < collection.length 
-    yield collection[i]
+    collection_array << yield collection[i]
     i += 1 
   end
   collection_array 
