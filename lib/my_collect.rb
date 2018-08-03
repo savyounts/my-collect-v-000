f10940e5a4baa0f338582dae3ebbf303d3665f0f
Sav Younts <savyounts@gmail.com>
2018-08-03 20:45:27 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  i = 0 
+  collection_array = []
+  while i < collection.length 
+    collection_array << yield(collection[i])
+    i += 1 
+  end
+  collection_array 
+end
